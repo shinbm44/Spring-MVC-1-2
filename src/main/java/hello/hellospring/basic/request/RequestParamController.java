ed3a1fd084099dae8bc2ecedf11a55469c8cde71
hyunjin_byun <shinbm219@naver.com>
2024-01-10 01:34:01 +0900

    [섹션6]스프링ãmvc 서블릿 파라미터

---
+++ src/main/java/hello/hellospring/basic/request/RequestParamController.java
@@ -0,0 +1,26 @@
+package hello.hellospring.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    // 서블릿 방식으로 작성한 http 쿼리 파라미터 전달 방식
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+}
